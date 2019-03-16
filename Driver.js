bb332a44652c73dc681716992c632a3685c3ffeb
Pamela Kepa <pkepa@outlook.com>
2019-03-16 14:36:49 +0100

    Update Driver.js

--- Driver.js
+++ Driver.js
@@ -5,12 +5,15 @@
 
 console.log("Driver.js loaded")
 class User{
-    constructor(firstName, lastName, username, password){
+    constructor(firstName, lastName, username, email, DofB, password, repeatPassword){
         //in the brackets we write it to specify order of input
         this.firstName = firstName;
         this.lastName = lastName;
-        this.username= username;
+        this.username = username;
+        this.email= email;
+        this.DofB= DofB;
         this.password = password;
+        this.repeatPassword=repeatPassword
         // we didn't create a checkbox constructor to minimize coding. If user didn't check it, he would't create account
     }
 }
