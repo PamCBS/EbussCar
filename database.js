80c40da28ee3f86b033fd90b4ebd0a451161ece8
Pamela Kepa <pkepa@outlook.com>
2019-03-16 14:36:44 +0100

    Update database.js

--- database.js
+++ database.js
@@ -6,4 +6,4 @@ var  users = []
 //users.puch adds new users to the array, that means that it will be saved only for one session
 // local storage sits in the browser, works like cookies.
 
-users.push(new User("Henrik", "Thorn", "hacker", "abc123"))
+users.push(new User("Henrik", "Thorn", "hacker", "henrik.thorn@student.cbs.dk", "01/15/1980", "abc123", "abc123"))
