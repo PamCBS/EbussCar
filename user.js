84b3040820705d13c833dce9a1fe87fc5f0a22fa
Pamela Kepa <pkepa@outlook.com>
2019-03-18 12:36:57 +0100

    Create user.js

---
+++ user.js
@@ -0,0 +1,18 @@
+//for driver's data: 
+//driver is an object, where we store information about name, birthdate, car, email etc
+//for every class that you create, build a individual JS.
+
+console.log("User.js loaded")
+class User{
+    constructor(firstName, lastName, username, email, DofB, password, repeatPassword){
+        //in the brackets we write it to specify order of input
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email= email;
+        this.DofB= DofB;
+        this.password = password;
+        this.repeatPassword=repeatPassword
+        // we didn't create a checkbox constructor to minimize coding. If user didn't check it, he would't create account
+    }
+}
