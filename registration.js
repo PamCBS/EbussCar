bab42672ff94f02a514819d43c203e82708b885d
Pamela Kepa <pkepa@outlook.com>
2019-03-19 10:54:30 +0100

    Update registration.js

--- registration.js
+++ registration.js
@@ -14,6 +14,7 @@ var DofBInput=document.getElementById("date-of-birth")
 var passwordInput=document.getElementById("password")
 var repeatPasswordInput=document.getElementById("repeat-password")
 var termsInput=document.getElementById("terms")
+var resultSpan = document.getElementById('loginResult');
 console.log(users)
 //just to check that we have at least one registered user (hardcoded)
 
@@ -22,48 +23,57 @@ console.log(users)
 //create a new user and add to array
 
 
-function conditions(){
-    // make sure that user fills  all fields  
-    if (firstNameInput.value.length === 0 || lastNameInput.value.length === 0 ||usernameInput.value.length === 0 || emailInput.value.length === 0 || DofBInput.value.length === 0 || passwordInput.value.length === 0 || repeatPasswordInput.value.length === 0) {
-        alert('You need to fill out all fields in order to sign up');
-        return false;
-    }
-   
-    //We already checked if there is any password entered but now we check if the password has at least 8 characters
-    if(passwordInput.value.length <=7){
-        alert("The password must be at least 8 characters.")
-      return false  
-    };
-    
-    if(passwordInput.value !== repeatPasswordInput.value){
-        alert("Your password does not match. Please try again.")
-    };
-     
-    if(emailInput.value.endsWith("@student.cbs.dk")!==false){
-        return true
-        }
-    else{
-    alert("This is not CBS student email. Please try again.")
-    };
-
-    termsChecked();
-}
-
 submitButton.onclick = function(){
     console.log("clicked")
-    conditions()
-    users.push(new User(firstNameInput.value, lastNameInput.value, usernameInput.value, emailInput.value, DofBInput.value, passwordInput.value, repeatPasswordInput.value))
-    console.log(users)
-}
+        // make sure that user fills all fields  
+    if (firstNameInput.value.length === 0 || lastNameInput.value.length === 0 ||usernameInput.value.length === 0 || emailInput.value.length === 0 || DofBInput.value.length === 0 || passwordInput.value.length === 0 || repeatPasswordInput.value.length === 0) {
+        resultSpan.innerText ='You need to fill out all fields in order to sign up';
+        return false;
+    };
+       //We already checked if there is any password entered but now we check if the password has at least 8 characters
 
-function termsChecked(){
-    if (termsInput.checked==true){
-        return true
-    } else{
-        alert ("You must accept Terms and Conditions to complete registration")
+    if(emailInput.value.endsWith("@student.cbs.dk")===false){
+        resultSpan.innerText ="This is not CBS student email. Please try again."
+        return false
     }
-}
 
-//function registrationCompleted(){
-//    for (user=1)
-//}
+    if(passwordInput.value.length <=7){
+        resultSpan.innerText ="The password must be at least 8 characters."
+      return false  
+    }
+
+    // check if password and confirm password match  
+    //If the passwords do not match, both fields are erased    
+    if(passwordInput.value !== repeatPasswordInput.value){
+        passwordInput.value=""
+        repeatPasswordInput.value=""
+        resultSpan.innerText ="Your password does not match. Please try again."
+        return false
+    };
+
+        // check if the email address is already stored in local storage
+    for (let i=0; i<users.length; i++){
+        if (emailInput.value===users[i].email){
+            resultSpan.innerText="The email has already been used, try to log in"
+            return false
+        };
+    };
+    
+    if(termsInput.checked){
+        var firstName= firstNameInput.value
+        var lastName = lastNameInput.value
+        var username = usernameInput.value
+        var email= emailInput.value
+        var DofB = DofBInput.value
+        var password = passwordInput.value
+
+        users.push(new User(firstName, lastName, username, email, DofB, password))
+        console.log(users)
+    }else {
+        resultSpan.innerText ="You must accept Terms and Conditions to complete registration"
+        return false
+    }     
+}//close functions
+
+// store sign up input in new user
+
