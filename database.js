37acd575e54c815cfae3e8ab3cdd3b3f277fdff0
Pamela Kepa <pkepa@outlook.com>
2019-03-21 10:51:33 +0100

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-
-console.log("database.js loaded")
-
-var  users = []
-//equals an empty array to save new users
-//users.puch adds new users to the array, that means that it will be saved only for one session
-// local storage sits in the browser, works like cookies.
-
-users.push(new User("Henrik", "Thorn", "hacker", "henrik.thorn@student.cbs.dk", "01/15/1980", "abc12345", "abc12345"))
