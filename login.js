df2f6dd1217373ffc8455d016985c950c9b9a3eb
Pamela Kepa <pkepa@outlook.com>
2019-03-21 13:17:41 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,15 @@
-var emailLoginInput=document.getElementById("login-email");
-var passwordLoginInput=document.getElementById("login-password");
-var resultSpan = document.getElementById("resultSpan");
-var loginButton = document.getElementById("login-btn");
+//by using const, we have variables with global scope.
+const emailLoginInput=document.getElementById("login-email");
+const passwordLoginInput=document.getElementById("login-password");
+const resultSpan = document.getElementById("resultSpan");
+const loginButton = document.getElementById("login-btn");
+const rememberMe=document.getElementById("remember-me");
 
 console.log("login.js is loaded")
 
 loginButton.onclick = function(){
 
-  console.log("loaded0")
+  console.log("loaded0")//debugger
 
   if(emailLoginInput.value.length == 0 || passwordLoginInput.value.length == 0){
     // We set the resultspan with a new text (old is in html) and return false to get out of this function
@@ -34,6 +36,10 @@ loginButton.onclick = function(){
   }
 
   showPassword()
+
+  if (rememberMe.checked){
+    e.preventDefault(); //to tell the browser not to refresh
+  }
 }
 function showPassword() {
     //this function allows User to see the password that he/she typed 
