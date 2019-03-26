3bb5e581e057f75c58abb810b683ee0105a79248
Pamela Kepa <pkepa@outlook.com>
2019-03-26 10:17:42 +0100

    Update userDatabase.js

--- userDatabase.js
+++ userDatabase.js
@@ -6,4 +6,13 @@ var  users = []
 //users.push adds new users to the array, that means that it will be saved only for one session
 // local storage sits in the browser, works like cookies.
 
-users.push(new User("Henrik", "Thorn", "hacker", "henrik.thorn@student.cbs.dk", "01/15/1980", "abc12345", "abc12345"))
+
+if (localStorage.getItem("users") === null) {
+    users = [new User("Henrik", "Thorn", "hacker", "henrik.thorn@student.cbs.dk", "01/15/1980", "abc12345", "abc12345")];
+} else {
+    users = JSON.parse(localStorage.getItem("users"));
+    for (let i = 0; i < users.length; i++) {
+        users[i] = new User(users[i].firstName, users[i].lastName, users[i].username,users[i].email, users[i].DofB, users[i].password);
+    }
+}
+
