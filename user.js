811594c338a3ab1c4b9e5ac2a68dc2088a48a00b
Pamela Kepa <pkepa@outlook.com>
2019-03-26 15:25:35 +0100

    Update user.js

--- user.js
+++ user.js
@@ -15,4 +15,20 @@ class User{
         this.repeatPassword=repeatPassword
         // we didn't create a checkbox constructor to minimize coding. If user didn't check it, he would't create account
     }
+    hashPassword(rawPassword){//function copy pasted from Jan, I asked and he said it's fine if we get his code.
+        var a = 1, c = 0, h, o;
+        if (rawPassword) {
+          a = 0;
+          for (h = rawPassword.length - 1; h >= 0; h--) {
+            o = rawPassword.charCodeAt(h);
+            a = (a<<6&268435455) + o + (o<<14);
+            c = a & 266338304;
+            a = c!==0?a^c>>21:a;
+          }
+        }else {
+          // If the password is not valid, we'll throw and error we're able to catch
+          throw new Error("The password supplied is not valid");
+        }
+        return String(a);
+        }
 }
