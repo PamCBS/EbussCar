9561a6f82225d9c467c70d66cd84cd63bcac5879
AlexG <alexandru.grund@gmail.com>
2019-03-29 10:06:29 +0100

    Create route.js

---
+++ route.js
@@ -0,0 +1,21 @@
+//for users' data: 
+//route is an object, where we store information about name, birthdate, car, email etc
+//for every class that you create, build a individual JS.
+
+console.log("Route.js loaded")
+
+
+class Route{
+    constructor(startLocation, endLocation, date, time, driverComission, basePrice, finalPrice, seats){
+        //in the brackets we write it to specify order of input
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
+        this.date = date;
+        this.time = time;
+        this.driverComission = driverComission;
+        this.basePrice = basePrice;
+        this.finalPrice = finalPrice;
+        this.seats = seats;
+
+    }
+}
