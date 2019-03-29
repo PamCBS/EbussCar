44aa61dbd62076f09a80e381f10e3edb717c6519
AlexG <alexandru.grund@gmail.com>
2019-03-29 10:06:35 +0100

    Create routeCreation.js

---
+++ routeCreation.js
@@ -0,0 +1,16 @@
+console.log("routeCreation.js loaded")
+
+//this is debugging tip
+
+// Bind the two input fields and get the value. By using const we make the scope of the variables global
+const submitButton = document.getElementById('submit-btn')
+const firstNameInput = document.getElementById("first-name")
+const lastNameInput = document.getElementById("last-name")
+const usernameInput = document.getElementById("username")
+const emailInput = document.getElementById("email")
+const DofBInput = document.getElementById("date-of-birth")
+const passwordInput = document.getElementById("password")
+const repeatPasswordInput = document.getElementById("repeat-password")
+const termsInput = document.getElementById("terms")
+const resultSpan = document.getElementById('loginResult');
+
