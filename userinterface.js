9f6c67a881f7bebe9fa44ba1698982612d88000c
Pamela Kepa <pkepa@outlook.com>
2019-05-04 19:17:24 +0200

    Update userinterface.js

--- userinterface.js
+++ userinterface.js
@@ -4,6 +4,8 @@ const divName = document.getElementById("profile-name");
 const divUsername = document.getElementById("profile-username");
 const divEmail = document.getElementById("profile-email");
 const divNoR = document.getElementById("profile-NofR");
+const deleteButton = document.getElementById("delete-btn")
+const resultSpan = document.getElementById("resultSpan")
 
 // get active user from ls
 // get all users from ls
@@ -20,7 +22,7 @@ const findCurrent = () => {
     const currentUser = allUsersLS.find((users) => {
         return users.email === activeUser
     })
-    
+
     divName.innerHTML = 'Name: ' + currentUser.firstName;
     divUsername.innerHTML = 'Username: ' + currentUser.username;
     divEmail.innerHTML = "Email: " + currentUser.email;
@@ -30,12 +32,29 @@ const findCurrent = () => {
 }
 findCurrent()
 
-if (users.email === activeUser){
-    for(let counter = 0; counter < allRidesLS.length; counter++){
-        counter ++;
+if (users.email === activeUser) {
+    for (let counter = 0; counter < allRidesLS.length; counter++) {
         divNoR = counter;
-        }
     }
+}
+
+deleteButton.onclick = function () {
+    console.log("delete button works")
+    confirm(`Hi ${divName}, are you sure you want to delete your account?`)
+    // the confirmation popup https://www.w3schools.com/jsref/met_win_confirm.asp
+    if (confirm == true) {
+        let indexcurrent = users.indexOf(currentUser)//syntax arr.indexOf(searchElement[, fromIndex])
+        if (indexcurrent > -1) {
+            users.splice(indexcurrent, 1);
+            window.location.href = "./registration.html";
+            return true
+        }
+    } else {
+        resultSpan.innerHTML = "Thank you for staying with us"
+        return false
+    }
+}
+
 
 //log out function
 const logoutButton = document.getElementById("logout-btn");
@@ -43,12 +62,12 @@ const logoutButton = document.getElementById("logout-btn");
 var isLoggedIn = JSON.parse(localStorage.getItem("isLoggedIn"))//
 console.log(`Is user logged in? ${isLoggedIn}`)
 //could go at the bottom of the .js
-logoutButton.onclick = function LogOut(){
-    if (isLoggedIn === true){
+logoutButton.onclick = function LogOut() {
+    if (isLoggedIn === true) {
         isLoggedIn = false;
         localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn)); // update local storage with setItem(key, value) value has to be string
         logoutButton.style.visibility = 'hidden';//after we log out, we do not want to see the log out button anymore
         console.log(`Is logged in?${isLoggedIn}`);
-        window.location.href="./login.html"
+        window.location.href = "./login.html"
     }
 }
