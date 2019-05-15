06853cf839a30c2c0b6d13f6c53f2a1d59c07034
AlexG <alexandru.grund@gmail.com>
2019-05-16 00:27:48 +0200

    Update route.js

--- route.js
+++ route.js
@@ -1,8 +1,7 @@
-//for users' data: 
-//route is an object, where we store information about name, birthdate, car, email etc
-//for every class that you create, build a individual JS.
+//for route's data: 
+//route is an object, where we store information about start location, end location, date, price etc.
 
-console.log("Route.js loaded")
+console.log("Route.js loaded")//debugger
 
 
 class Route{
@@ -16,7 +15,9 @@ class Route{
         this.basePrice = basePrice;
         this.finalPrice = finalPrice;
         this.seats = seats;
+        //just a dummy string input, it can be linked to the PayPal api in order to be used as a payment method
         this.payPal = payPal;
+        //owner will help attach the created route to the currently logged in user
         this.owner = owner;
 
     }
